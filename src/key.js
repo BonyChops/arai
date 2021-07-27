c9c7e4436250e6ae068ea1c2888bb01cb1b420ef
BonyChops <contact.bonychops@gmail.com>
2021-07-27 16:02:38 +0900

    Edit

---
+++ src/key.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyA7HFGP_a8SwQd6DYXP6hBcFHm6rBHI_ek",
+    authDomain: "araiapp.firebaseapp.com",
+    projectId: "araiapp",
+    storageBucket: "araiapp.appspot.com",
+    messagingSenderId: "560620256673",
+    appId: "1:560620256673:web:c3886d9a102209c79fdfe0",
+    measurementId: "G-NE4MP5WTP2"
+};
+
+export default firebaseConfig;
